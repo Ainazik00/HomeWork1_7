842c01353cfb46960fe4a69ae16b885013543015
Ainazik00 <ainazikkulchoroeva23@gmail.com>
2023-09-23 11:49:13 +0300

    Commit

---
+++ src/main/java/org/example/Warrior.java
@@ -0,0 +1,12 @@
+package org.example;
+
+class Warrior extends Hero {
+    public Warrior() {
+        super(150, 30, "CRITICAL DAMAGE");
+    }
+
+    @Override
+    public void applySuperAbility() {
+        System.out.println("Warrior применил суперспособность " + superAbility);
+    }
+}
